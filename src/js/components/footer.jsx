6715d7692457ecd2acec6d7938418230fa961378
Mariabel17 <maribelml17@gmail.com>
2025-11-30 07:49:52 +0000

    landing terminada

---
+++ src/js/components/footer.jsx
@@ -0,0 +1,12 @@
+const Footer = ()=>{
+
+    return(
+        
+    <footer className="bg-dark text-white text-center p-3" >
+      Created by Maribel Marín.
+    </footer>
+
+    )
+}
+
+export default Footer
---
+++ src/js/components/jumbotron.jsx
@@ -0,0 +1,19 @@
+const Jumbotron = () => {
+    
+
+    return (
+    <div className="p-5 mb-4 bg-light rounded-3">
+      <div className="container-fluid py-5">
+        <h1 className="display-5 fw-bold">Bienvenid@ a mi Landing Page</h1>
+        <p className="col-md-8 fs-4">
+          Esta es una página creada con React y Bootstrap. Aquí podrás ver un ejemplo de estructura con navbar, jumbotron, cards y footer.
+        </p>
+        <button className="btn btn-primary btn-lg" type="button">
+          Call to Action
+        </button>
+      </div>
+    </div>
+
+    )
+}
+export default Jumbotron
